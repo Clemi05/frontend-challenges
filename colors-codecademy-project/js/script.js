da44d10e4edec00a62d091d65c8680ccb710cd63
Clement Azalbert <clementazalbert04@gmail.com>
2022-02-10 23:32:00 +0100

    added js animation button

--- colors-codecademy-project/js/script.js
+++ colors-codecademy-project/js/script.js
@@ -1,6 +1,18 @@
-const colors = document.querySelectorAll('.color .reds');
+const buttonRed = document.querySelector('.btn-red');
+const buttonGreen = document.querySelector('.btn-green');
+const buttonBlue = document.querySelector('.btn-blue');
 
-colors.forEach(color => {
-  let redSwatches = color.querySelectorAll('.color .reds');
-  console.log(redSwatches);
+/* Toggle Red Swatches */
+buttonRed.addEventListener('click', () => {
+  document.getElementById('red').classList.toggle('toggled');
+});
+
+/* Toggle Green Swatches */
+buttonGreen.addEventListener('click', () => {
+  document.getElementById('green').classList.toggle('toggled');
+});
+
+/* Toggle Blue Swatches */
+buttonRed.addEventListener('click', () => {
+  document.getElementById('blue').classList.toggle('toggled');
 });
