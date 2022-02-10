0d915270ee3ab6534572c64ba0da6935ccec9a41
Clement Azalbert <clementazalbert04@gmail.com>
2022-02-10 23:49:07 +0100

    updated css files and structure

--- colors-codecademy-project/js/script.js
+++ colors-codecademy-project/js/script.js
@@ -4,15 +4,21 @@ const buttonBlue = document.querySelector('.btn-blue');
 
 /* Toggle Red Swatches */
 buttonRed.addEventListener('click', () => {
-  document.getElementById('red').classList.toggle('toggled');
+  document.getElementById('red-lightness').classList.toggle('toggled');
+  document.getElementById('red-saturation').classList.toggle('toggled');
+  document.getElementById('red-hue').classList.toggle('toggled');
 });
 
 /* Toggle Green Swatches */
 buttonGreen.addEventListener('click', () => {
-  document.getElementById('green').classList.toggle('toggled');
+  document.getElementById('green-lightness').classList.toggle('toggled');
+  document.getElementById('green-saturation').classList.toggle('toggled');
+  document.getElementById('green-hue').classList.toggle('toggled');
 });
 
 /* Toggle Blue Swatches */
 buttonRed.addEventListener('click', () => {
-  document.getElementById('blue').classList.toggle('toggled');
+  document.getElementById('blue-lightness').classList.toggle('toggled');
+  document.getElementById('blue-saturation').classList.toggle('toggled');
+  document.getElementById('blue-hue').classList.toggle('toggled');
 });
